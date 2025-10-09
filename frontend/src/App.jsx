4be73fcdb7b78ece250e3a6b7ea24f0307d21180
Thando Fredericks <st10187895@vcconnect.edu.za>
2025-10-09 12:19:36 +0200

    trying to get this thing to work

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,21 @@ import Topbar from "./components/Topbar";
 import AddBeneficiaryForm from "./components/AddBeneficiaryForm";
 import PurchaseAirtimeDetails from "./components/PurchaseAirtimeDetails";
 import BuyElectricityWater from "./components/BuyElectricityWater";
+import DashboardLayout from "./components/DashboardLayout";
+import Dashboard from "./components/Dashboard";
 import "./components/AddBeneficiary.css";
 import "./components/PurchaseAirtimeDetails.css";
 import "./components/BuyElectricityWater.css";
+import "./components/DashboardLayout.css";
+import "./components/Sidebar.css"
+import "./components/Topbar.css";
 import "./App.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <Sidebar />
-      <div className="main-section">
-        <Topbar />
-        <BuyElectricityWater />
-      </div>
-    </div>
+    <DashboardLayout>
+      <BuyElectricityWater />
+    </DashboardLayout>
   );
 }
 
